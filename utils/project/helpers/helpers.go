545049edede2ca6ee1b51028c76cf30b8ca8ca50
Hugues Dubois <hugdubois@gmail.com>
2018-10-24 18:36:41 +0200

    Update retool gomeet's revision

--- utils/project/helpers/helpers.go
+++ utils/project/helpers/helpers.go
@@ -15,7 +15,7 @@ import (
 
 const (
 	GomeetDefaultPrefixes = "svc-,gomeet-svc-"
-	GomeetRetoolRev       = "ce5d6eeb06c0ddcb74ed7f33c7a11ef301365053"
+	GomeetRetoolRev       = "b261e101ba6e62219541fdbae889deb86e5e9592"
 )
 
 type Empty struct{}
