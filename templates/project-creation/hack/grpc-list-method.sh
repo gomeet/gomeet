1dc9dd94f48b50fb25b3754f18295d450a73d870
Hugues Dubois <hugdubois@gmail.com>
2018-11-06 12:22:56 +0100

    Some improvements

--- templates/project-creation/hack/grpc-list-method.sh
+++ templates/project-creation/hack/grpc-list-method.sh
@@ -3,6 +3,6 @@
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 BASE_DIR=$SCRIPTPATH/..
-SVC_PROTO=$BASE_DIR/{{ .GoProtoPkgAlias }}/{{ .ShortName }}.proto
+SVC_PROTO=$BASE_DIR/pb/{{ .ShortName }}.proto
 
 grep -oP "rpc \K(.*)\(.*\) returns" $SVC_PROTO | sed -n -e 's/^\([[:alnum:]]\+\).*/\1/p'
