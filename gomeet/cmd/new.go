c1cb6612f37d2e07cb0a3f41911869b151f3872f
Hugues Dubois <hugdubois@gmail.com>
2018-11-15 12:52:43 +0100

    Added silent flag to new command

--- gomeet/cmd/new.go
+++ gomeet/cmd/new.go
@@ -14,6 +14,13 @@ import (
 	"github.com/gomeet/gomeet/utils/project"
 )
 
+type askOptions int
+
+const (
+	DEFAULT_NO askOptions = iota
+	DEFAULT_YES
+)
+
 type FinishMsgParams struct {
 	Path string
 	Arg  string
@@ -36,6 +43,7 @@ var (
 	defaultPrefixes string
 	protoAlias      string
 	force           bool
+	silent          bool
 	noGogo          bool
 	dbTypes         string
 	uiType          string
@@ -58,6 +66,7 @@ func init() {
 	newCmd.PersistentFlags().StringVar(&queueTypes, "queue-types", "", fmt.Sprintf("Queue types [%s] (comma separated)", strings.Join(project.GomeetAllowedQueueTypes(), ",")))
 	newCmd.PersistentFlags().StringVar(&extraServeFlags, "extra-serve-flags", "", "extra serve flags passed to gRPC server format [<name-of-flag>@<type-of-flag[string|int]>|<flag description (no comma, no semicolon, no colon)>|<default value>] (comma separated)")
 	newCmd.PersistentFlags().StringVar(&cronTasks, "cron-tasks", "", "Cron tasks (comma separated)")
+	newCmd.PersistentFlags().BoolVarP(&silent, "silent", "s", false, "Silent questions with default answers")
 
 	rootCmd.AddCommand(newCmd)
 }
@@ -68,6 +77,10 @@ func new(cmd *cobra.Command, args []string) {
 		return
 	}
 
+	if os.Getenv("CI") != "" {
+		silent = true
+	}
+
 	name := args[0]
 	p, err := project.New(name)
 	if err != nil {
@@ -75,7 +88,7 @@ func new(cmd *cobra.Command, args []string) {
 	}
 
 	fmt.Printf("Creating project in %s\n", p.Path())
-	if !askIsOK("Is this OK?") {
+	if !silent && !askIsOK("Is this OK?", DEFAULT_NO) {
 		fmt.Println("Exiting..")
 		return
 	}
@@ -142,10 +155,9 @@ func new(cmd *cobra.Command, args []string) {
 	}
 
 	keepProtoModel := true
-	if force {
-		keepProtoModel = !askIsOK("Are you sure you want to overwrite the protobuf, models, tools, package, ui files, auth_and_acl_funcs and cli helpers files ?")
+	if !silent && force {
+		keepProtoModel = !askIsOK("Keep the protobuf, models, tools, package, ui files, auth_and_acl_funcs and helpers files ?", DEFAULT_YES)
 	}
-
 	p.UseGogoGen(!noGogo)
 
 	// create new project
@@ -158,7 +170,7 @@ func new(cmd *cobra.Command, args []string) {
 		return
 	}
 
-	if askIsOK("Print tree?") {
+	if silent || askIsOK("Print tree?", DEFAULT_NO) {
 		p.PrintTreeFolder()
 	}
 
@@ -179,7 +191,7 @@ func new(cmd *cobra.Command, args []string) {
 		er(err)
 	}
 
-	if !askIsOK("Do it?") {
+	if !silent && !askIsOK("Do it?", DEFAULT_NO) {
 		fmt.Println("Exiting..")
 		return
 	}
@@ -203,7 +215,7 @@ func new(cmd *cobra.Command, args []string) {
 		er(err)
 	}
 
-	if !askIsOK("Do it?") {
+	if !silent && !askIsOK("Do it?", DEFAULT_NO) {
 		fmt.Println("Exiting..")
 		return
 	}
@@ -213,9 +225,17 @@ func new(cmd *cobra.Command, args []string) {
 	}
 }
 
-func askIsOK(msg string) bool {
-	if os.Getenv("CI") != "" {
-		return true
+func askIsOK(msg string, defaultVal askOptions) bool {
+	var (
+		yTxt, nTxt string
+		checkVal   string
+	)
+
+	switch defaultVal {
+	case DEFAULT_NO:
+		checkVal, yTxt, nTxt = "y", "[y]", "[N]"
+	case DEFAULT_YES:
+		checkVal, yTxt, nTxt = "n", "[Y]", "[n]"
 	}
 
 	if msg == "" {
@@ -224,13 +244,13 @@ func askIsOK(msg string) bool {
 
 	fmt.Fprintf(out, "\n%s\n%ses/%so\n",
 		msg,
-		color.YellowString("[y]"),
-		color.CyanString("[N]"),
+		color.YellowString(yTxt),
+		color.CyanString(nTxt),
 	)
 
 	scan := bufio.NewScanner(os.Stdin)
 	scan.Scan()
-	return strings.Contains(strings.ToLower(scan.Text()), "y")
+	return strings.Contains(strings.ToLower(scan.Text()), checkVal)
 }
 
 func er(err error) {
